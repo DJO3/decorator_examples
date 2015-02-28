72e455fb1e6c2a15a21ad7d37c26949c15e14fe0
Dave O'Connor <oconnorwork@gmail.com>
2015-02-28 13:19:58 -0500

    add decorator_add.py

---
+++ decorator_add.py
@@ -0,0 +1,17 @@
+# Adds 5 to desired function
+def add5(function):
+    def wrapper(*args):
+        return function(*args) + 5
+    return wrapper
+
+
+@add5
+def multiply(num1, num2):
+    return num1 * num2
+
+if __name__ == "__main__":
+    print 'Please enter two numbers to multiply.' 
+    num1 = int(raw_input('Number 1: '))
+    num2 = int(raw_input('Number 2: '))
+    print 'Before decorator: {0}'.format((num1 * num2))
+    print 'After add5 decorator: {0}'.format(( multiply(num1, num2)))
